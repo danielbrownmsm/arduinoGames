f2d482aeed817bdf1b1d0cd888fad58feb8cdb1d
danielbrownmsm <56051347+danielbrownmsm@users.noreply.github.com>
2020-09-06 15:26:24 -0500

    continued refactoring

--- matrixGame.ino
+++ matrixGame.ino
@@ -19,25 +19,18 @@ int highThreshold = 800;
 int lowThreshold = 200;
 int x = 1;
 int y = 1;
-int prev_x = 0;
-int prev_y = 0;
 int buttonVal;
 unsigned int longPressThreshold = 1000;
 bool lastState;
 unsigned long lastTime;
 unsigned long currTime;
-
-byte screen[] = {B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000};
-
-// Tic-Tac-Toe / Knots + Crosses
-int ticTacToeBoard[] = {0, 0, 0, 0, 0 , 0, 0, 0, 0};
-int rowSize = 3;
-int columnSize = 3;
 bool blinkState = true;
 
+byte screen[8] = {B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000};
+
 // Game of Life
-byte gol_board[] = {B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000};
-byte prev_board[] = {B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000};
+byte gol_board[8] = {B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000};
+byte prev_board[8] = {B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000};
 bool runningSim = false;
 int numNeighbors;
 int minusColumn;
@@ -46,6 +39,12 @@ int minusRow;
 int plusRow;
 
 LedControl matrix = LedControl(dinPin, clkPin, csPin, 1); // 1 b/c only using the one matrix
+
+void flipDisplay(LedControl matrix, byte screen[]) {
+  for (int i = 0; i < 8; i++) {
+    matrix.setRow(0, i, screen[i]);
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -67,65 +66,58 @@ void setup() {
   y = 4;
 }
 
+void byteToXY(int val) {
+  return map(val, 0, 7, 7, 0);
+}
+
 void loop() {
-  if (!runningSim) {
-    while (!runningSim) {
-      blinkState = !blinkState;
+  while (!runningSim) {
+    blinkState = !blinkState; // toggle the blinking LED
 
-      flipScreen(matrix, screen);
-      matrix.setLed(0, x, map(y, 0, 7, 7, 0), blinkState); // map b/c y is 0...7 while bytes is 7...0 (read from left to right)
+    memcpy(screen, gol_board, sizeof(gol_board)) // move world data to screen
+    flipDisplay(matrix, screen); // flip
+    matrix.setLed(0, x, byteToXY(y), blinkState); // map b/c y is 0...7 while bytes is 7...0 (read from left to right)
 
-      x_val = analogRead(xPin);
-      if (x_val > highThreshold and x < 7) {
-        x += 1;
-      }
-      else if (x_val < lowThreshold and x > 0) {
-        x -= 1;
-      }
-
-      y_val = analogRead(yPin);
-      if (y_val > highThreshold and y < 7) {
-        y += 1;
-      }
-      else if (lowThreshold < 300 and y > 0) {
-        y -= 1;
-      }
-
-      buttonVal = digitalRead(buttonPin);
-      if (buttonVal == LOW) {
-        if (lastState == HIGH) {
-          lastState = LOW;
-          lastTime = millis();
-        }
-        currTime = millis();
-
-        if (currTime - lastTime > longPressThreshold) { // if long press
-          runningSim = true; // start the simulation
-        }
-
-        bitWrite(screen[x], y, (bitRead(screen[x], y) ^ 1)); // toggle the bit at that loc
-      }
-      else {
-        lastState = HIGH;
-      }
-
-      prev_x = x;
-      prev_y = y;
-
-      delay(200);
+    x_val = analogRead(xPin);
+    if (x_val > highThreshold and x < 7) {
+      x += 1;
+    } else if (x_val < lowThreshold and x > 0) {
+      x -= 1;
     }
+
+    y_val = analogRead(yPin);
+    if (y_val > highThreshold and y < 7) {
+      y += 1;
+    } else if (lowThreshold < 300 and y > 0) {
+      y -= 1;
+    }
+
+    buttonVal = digitalRead(buttonPin);
+    if (buttonVal == LOW) {
+      if (lastState == HIGH) {
+        lastState = LOW;
+        lastTime = millis();
+      }
+      currTime = millis();
+
+      if (currTime - lastTime > longPressThreshold) { // if long press
+        runningSim = true; // start the simulation
+      }
+
+      bitWrite(gol_board[x], y, (bitRead(gol_board[x], y) ^ 1)); // toggle the bit at that loc
+    } else {
+      lastState = HIGH;
+    }
+    delay(200);
   }
 
-  /**
-   * TODO: FIX THIS!
-  */
-  for (int x = 0; x < 8; x++) {
+/*  for (int x = 0; x < 8; x++) {
     for (int y = 0; y < 8; y++) {
       gol_board[x][y] = screen[x][bitRead(screen[x], map(y, 0, 7, 7, 0))];
       prev_board[x][y] = gol_board[x][y];
-      bitWrite(screen[x], map(y, 0, 7, 7, 0), gol_board[x][y]);
+      bitWrite(screen[x], byteToXY(y), gol_board[x][y]);
     }
-  }
+  }*/
 
   flipDisplay(matrix, screen);
 
@@ -168,7 +160,7 @@ void loop() {
       numNeighbors += prev_board[row + plusRow][column - minusColumn];
       numNeighbors += prev_board[row + plusRow][column];
       numNeighbors += prev_board[row + plusRow][column + plusColumn];
-      
+
       if (numNeighbors < 2 or numNeighbors > 3) { // if it doesn't have 2-3 neighbors
         gol_board[row][column] = false; // it dies
       } else { // else they continue living / are born
@@ -177,10 +169,4 @@ void loop() {
     }
   }
   delay(200);
-}
-
-void flipDisplay(LedControl matrix, byte[] screen) {
-  for (int i = 0; i < 8; i++) {
-    matrix.setRow(0, i, screen[i]);
-  }
 }
