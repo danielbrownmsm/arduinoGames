3e58bf22351bde4e9169e915a3676eb5abb28f55
danielbrownmsm <56051347+danielbrownmsm@users.noreply.github.com>
2020-09-07 20:28:33 -0500

    created blackjack
    It doesn't work right though so fix plz. Also make a menu. And fix GoL. But yeah today was pretty good

--- matrixGame.ino
+++ matrixGame.ino
@@ -38,6 +38,45 @@ int plusColumn;
 int minusRow;
 int plusRow;
 
+// blackjack
+int p_total = 0;
+int c_total = 0;
+int c_threshold = 17;
+long newCard = 0;
+bool hit = false;
+bool decided = false;
+
+// symbols
+byte plusSign[8] = {B00000000, B00011000, B00011000, B01111110, B01111110, B00011000, B00011000, B00000000}; // plus symbol
+byte minusSign[8] = {B00000000, B00000000, B00000000, B01111110, B01111110, B00000000, B00000000, B00000000}; // minus symbol
+byte happy[8] = {B00000000, B01100110, B01100110, B00000000, B00000000, B01000010, B00111100, B00000000}; // happy face
+byte sad[8] = {B00000000, B01100110, B01100110, B00000000, B00000000, B00111100, B01000010, B00000000}; // sad face
+
+// numerals
+byte one[5] = {B00000001, B00000011, B00000001, B00000001, B00000001}; // numeral 1
+byte two[5] = {B00000111, B00000001, B00000111, B00000100, B00000111}; // numeral 2
+byte three[5] = {B00000111, B00000001, B00000111, B00000001, B00000111}; // numeral 3
+byte four[5] = {B00000101, B00000101, B00000111, B00000001, B00000001}; // numeral 4
+byte five[5] = {B00000111, B00000100, B00000111, B00000001, B00000111}; // numeral 5
+byte six[5] = {B00000111, B00000100, B00000111, B00000101, B00000111}; // numeral 6
+byte seven[5] = {B00000111, B00000001, B00000010, B00000010, B00000100}; // numeral 7
+byte eight[5] = {B00000111, B00000101, B00000111, B00000101, B00000111}; // numeral 8
+byte nine[5] = {B00000111, B00000101, B00000111, B00000001, B00000111}; // numeral 9
+byte zero[5] = {B00000111, B00000101, B00000101, B00000101, B00000111}; // numeral 0
+byte digits[10][5] = {{B00000111, B00000101, B00000101, B00000101, B00000111},
+                    {B00000001, B00000011, B00000001, B00000001, B00000001}, 
+                    {B00000111, B00000001, B00000111, B00000100, B00000111}, 
+                    {B00000111, B00000001, B00000111, B00000001, B00000111}, 
+                    {B00000101, B00000101, B00000111, B00000001, B00000001}, 
+                    {B00000111, B00000100, B00000111, B00000001, B00000111}, 
+                    {B00000111, B00000100, B00000111, B00000101, B00000111}, 
+                    {B00000111, B00000001, B00000010, B00000010, B00000100}, 
+                    {B00000111, B00000101, B00000111, B00000101, B00000111}, 
+                    {B00000111, B00000101, B00000101, B00000101, B00000111}}; // everything at it's index
+
+// menu
+String game = "gol";
+
 LedControl matrix = LedControl(dinPin, clkPin, csPin, 1); // 1 b/c only using the one matrix
 
 void setup() {
@@ -55,9 +94,7 @@ void setup() {
   matrix.setIntensity(0, 0); // b/c LEDs can be _very_ brigt (and also draw current)
   matrix.clearDisplay(0);
 
-  // really temp-temp (b/c other games might need diff x/y start vals
-  x = 4;
-  y = 4;
+  randomSeed(analogRead(5)); // seed PRNG with floating pin (unless need be A5? (not to be confused with @55/A$$))
 }
 
 int byteToXY(int val) { // really I would say Cartesian but who has space to write that?
@@ -65,108 +102,202 @@ int byteToXY(int val) { // really I would say Cartesian but who has space to wri
 }
 
 void loop() {
-  while (!runningSim) { // before we are running the game and are still setting up
-    blinkState = !blinkState; // toggle the blinking LED
+  if (game == "gol") {
+    while (!runningSim) { // before we are running the game and are still setting up
+      blinkState = !blinkState; // toggle the blinking LED
+
+      memcpy(screen, gol_board, sizeof(gol_board)); // move world data to screen
+      for (int i = 0; i < 8; i++) {
+        matrix.setRow(0, i, screen[i]); // flip
+      }
+      matrix.setLed(0, x, byteToXY(y), blinkState); // map b/c y is 0...7 while bytes is 7...0 (read from left to right)
+      
+      // get joystick vals
+      x_val = analogRead(xPin);
+      if (x_val > highThreshold and x < 7) {
+        x += 1; // update accordingly
+      } else if (x_val < lowThreshold and x > 0) {
+        x -= 1;
+      }
+
+      y_val = analogRead(yPin);
+      if (y_val > highThreshold and y < 7) {
+        y += 1;
+      } else if (y_val < lowThreshold and y > 0) {
+        y -= 1;
+      }
+
+      buttonVal = digitalRead(buttonPin);
+      if (buttonVal == LOW) { // this is for figuring out if it is a long or short press
+        if (lastState == HIGH) {
+          lastState = LOW;
+          lastTime = millis();
+        }
+        currTime = millis();
+
+        if (currTime - lastTime > longPressThreshold) { // if long press
+          runningSim = true; // start the simulation
+          continue; // we don't want to toggle the bit at the location of the long press
+        }
+
+        bitWrite(gol_board[x], y, (bitRead(gol_board[x], y) ^ 1)); // toggle the bit at that loc
+      } else {
+        lastState = HIGH;
+      }
+      delay(200);
+    }
 
     memcpy(screen, gol_board, sizeof(gol_board)); // move world data to screen
-    for (int i = 0; i < 8; i++) {
-      matrix.setRow(0, i, screen[i]); // flip
+    memcpy(prev_board, gol_board, sizeof(gol_board)); // archive world data (basically not really)
+    for (int i = 0; i < 8; i++) { // flip
+      matrix.setRow(0, i, screen[i]);
     }
-    matrix.setLed(0, x, byteToXY(y), blinkState); // map b/c y is 0...7 while bytes is 7...0 (read from left to right)
     
-    // get joystick vals
-    x_val = analogRead(xPin);
-    if (x_val > highThreshold and x < 7) {
-      x += 1; // update accordingly
-    } else if (x_val < lowThreshold and x > 0) {
-      x -= 1;
-    }
+    for (int row = 0; row < 8; row++) { // for each row
+      for (int column = 0; column < 8; column++ ) {
+        int numNeighbors = 0;
 
-    y_val = analogRead(yPin);
-    if (y_val > highThreshold and y < 7) {
-      y += 1;
-    } else if (y_val < lowThreshold and y > 0) {
-      y -= 1;
-    }
-
-    buttonVal = digitalRead(buttonPin);
-    if (buttonVal == LOW) { // this is for figuring out if it is a long or short press
-      if (lastState == HIGH) {
-        lastState = LOW;
-        lastTime = millis();
-      }
-      currTime = millis();
-
-      if (currTime - lastTime > longPressThreshold) { // if long press
-        runningSim = true; // start the simulation
-        continue; // we don't want to toggle the bit at the location of the long press
-      }
-
-      bitWrite(gol_board[x], y, (bitRead(gol_board[x], y) ^ 1)); // toggle the bit at that loc
-    } else {
-      lastState = HIGH;
-    }
-    delay(200);
-  }
-
-  memcpy(screen, gol_board, sizeof(gol_board)); // move world data to screen
-  memcpy(prev_board, gol_board, sizeof(gol_board)); // archive world data (basically not really)
-  for (int i = 0; i < 8; i++) { // flip
-    matrix.setRow(0, i, screen[i]);
-  }
-  
-  for (int row = 0; row < 8; row++) { // for each row
-    for (int column = 0; column < 8; column++ ) {
-      int numNeighbors = 0;
-
-      /** plus/minus column/row is for edges so it wraps around. I may have not implemented it right though.
-       * World is supposed to be a torus
-       */
-      if (column == 0) {
-        minusColumn = -7;
-      } else {
-        minusColumn = 1;
-      }
-
-      if (column == 7) {
-        plusColumn = -7;
-      } else {
-        plusColumn = 1;
-      }
-
-      if (row == 7) {
-        plusRow = -7;
-      } else {
-        plusRow = 1;
-      }
-
-      if (row == 0) {
-        minusRow = -7;
-      } else {
-        minusRow = 1;
-      }
-
-
-      numNeighbors += bitRead(prev_board[row - minusRow], column - minusColumn);
-      numNeighbors += bitRead(prev_board[row - minusRow], column);
-      numNeighbors += bitRead(prev_board[row - minusRow], column + plusColumn);
-
-      numNeighbors += bitRead(prev_board[row], column - minusColumn);
-      numNeighbors += bitRead(prev_board[row], column + plusColumn);
-
-      numNeighbors += bitRead(prev_board[row] + plusRow, column - minusColumn);
-      numNeighbors += bitRead(prev_board[row] + plusRow, column);
-      numNeighbors += bitRead(prev_board[row] + plusRow, column + plusColumn);
-            
-      if (numNeighbors > 1 and numNeighbors < 4) { // if it has 2-3 neighbors
-        if (numNeighbors == 3) {
-          bitSet(gol_board[row], column); // it is born if it has 3
+        /** plus/minus column/row is for edges so it wraps around. I may have not implemented it right though.
+         * World is supposed to be a torus
+         */
+        if (column == 0) {
+          minusColumn = -7;
+        } else {
+          minusColumn = 1;
         }
-        // otherwise (for nested if) nothing happens (keeps state)
-      } else { // else they continue living / are born
-        bitClear(gol_board[row], column); // it dies
+
+        if (column == 7) {
+          plusColumn = -7;
+        } else {
+          plusColumn = 1;
+        }
+
+        if (row == 7) {
+          plusRow = -7;
+        } else {
+          plusRow = 1;
+        }
+
+        if (row == 0) {
+          minusRow = -7;
+        } else {
+          minusRow = 1;
+        }
+
+
+        numNeighbors += bitRead(prev_board[row - minusRow], column - minusColumn);
+        numNeighbors += bitRead(prev_board[row - minusRow], column);
+        numNeighbors += bitRead(prev_board[row - minusRow], column + plusColumn);
+
+        numNeighbors += bitRead(prev_board[row], column - minusColumn);
+        numNeighbors += bitRead(prev_board[row], column + plusColumn);
+
+        numNeighbors += bitRead(prev_board[row] + plusRow, column - minusColumn);
+        numNeighbors += bitRead(prev_board[row] + plusRow, column);
+        numNeighbors += bitRead(prev_board[row] + plusRow, column + plusColumn);
+              
+        if (numNeighbors > 1 and numNeighbors < 4) { // if it has 2-3 neighbors
+          if (numNeighbors == 3) {
+            bitSet(gol_board[row], column); // it is born if it has 3
+          }
+          // otherwise (for nested if) nothing happens (keeps state)
+        } else { // else they continue living / are born
+          bitClear(gol_board[row], column); // it dies
+        }
       }
     }
+    delay(500);
   }
-  delay(500);
+  else if (game == "21") {
+    // do blackjack here
+    p_total = random(1, 10);
+    p_total += random(1, 10);
+    
+    int onesDigit = p_total % 10;
+    int tensDigit = (p_total / 10) % 10; // because result is int it removes stuff after decimal
+    for (int i = 0; i < 5; i++) { // b/c we only have 5 rows of number to display
+      matrix.setRow(0, i+1, (digits[tensDigit][i] << 4) & digits[onesDigit][i]); // this _should_ work. Maybe. Honestly tho it prob won't
+    }
+
+    delay(1000);
+
+    // check hit or stay
+    while (!decided) {
+      y_val = analogRead(yPin);
+      if (y_val > highThreshold and y < 1) {
+        y += 1; // update accordingly
+      } else if (y_val < lowThreshold and y > 0) {
+        y -= 1;
+      }
+
+      if (y == 1) {
+        for (int i = 0; i < 8; i++) {
+          matrix.setRow(0, i, plusSign[i]);
+        }
+      } else {
+        for (int i = 0; i < 8; i++) {
+          matrix.setRow(0, i, minusSign[i]);
+        }
+      }
+
+      decided = digitalRead(buttonPin); // dang you can write stuff short!
+      hit = y == 1; // if y is one then yes hit otherwise no
+    }
+
+    while (hit) {
+      p_total += random(1, 10);
+      int onesDigit = p_total % 10;
+      int tensDigit = (p_total / 10) % 10; // because result is int it removes stuff after decimal
+      for (int i = 0; i < 5; i++) { // b/c we only have 5 rows of number to display
+        matrix.setRow(0, i+1, (digits[tensDigit][i] << 4) & digits[onesDigit][i]); // this _should_ work. Maybe. Honestly tho it prob won't
+      }
+      
+      decided = false;
+      // check hit or stay
+      while (!decided) {
+        y_val = analogRead(yPin);
+        if (y_val > highThreshold and y < 1) {
+          y += 1; // update accordingly
+        } else if (y_val < lowThreshold and y > 0) {
+          y -= 1;
+        }
+
+        if (y == 1) {
+          for (int i = 0; i < 8; i++) {
+            matrix.setRow(0, i, plusSign[i]);
+          }
+        } else {
+          for (int i = 0; i < 8; i++) {
+            matrix.setRow(0, i, minusSign[i]);
+          }
+        }
+
+        decided = digitalRead(buttonPin); // dang you can write stuff short!
+        hit = y == 1; // if y is one then yes hit otherwise no
+      }
+
+      if (p_total > 21) {
+        for (int i = 0; i < 8; i++) {
+          matrix.setRow(0, i, sad[i]);
+        }
+        break;
+      }
+    }
+
+    c_total = random(1, 10);
+    c_total += random(1, 10);
+    
+    while (c_total < c_threshold) {
+      c_total += random(1, 10); // computer hits if below its threshold
+    }
+    if (c_total > 21 || p_total > c_total) {
+      for (int i = 0; i < 8; i++) {
+        matrix.setRow(0, i, happy[i]);
+      }
+    } else { 
+      for (int i = 0; i < 8; i++) {
+        matrix.setRow(0, i, sad[i]);
+      }
+     }
+  }
 }
