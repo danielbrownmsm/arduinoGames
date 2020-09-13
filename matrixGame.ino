020da43c1018db6998da9eea4e2bb62a06ace5d0
danielbrownmsm <56051347+danielbrownmsm@users.noreply.github.com>
2020-09-13 14:03:33 -0500

    tower stack blocks is fully implemented and working now

--- matrixGame.ino
+++ matrixGame.ino
@@ -53,14 +53,12 @@ byte happy[8] = {B00000000, B01100110, B01100110, B00000000, B00000000, B0100001
 byte sad[8] = {B00000000, B01100110, B01100110, B00000000, B00000000, B00111100, B01000010, B00000000}; // sad face
 
 // tower stack blocks
-byte lastLevel = B011111110;
-byte newLevel;
-byte tower[7] = {};
-int write_loc = 0;
-int read_loc = 0;
-int tower_x = 0;
+byte tower[7] = {B00111111, B01111110, B01111110, B01111110, B01111110, B01111110, B01111110};
 int tower_y = 0;
 int direction = 0;
+unsigned long waitTime = 100;
+int score = 0;
+bool breakLoop = false;
 
 // numerals
 byte digits[10][5] = {{B00000111, B00000101, B00000101, B00000101, B00000111},
@@ -75,7 +73,7 @@ byte digits[10][5] = {{B00000111, B00000101, B00000101, B00000101, B00000111},
                     {B00000111, B00000101, B00000101, B00000101, B00000111}}; // everything at it's index
 
 // menu
-String game = "21";
+String game = "stac";
 
 LedControl matrix = LedControl(dinPin, clkPin, csPin, 1); // 1 b/c only using the one matrix
 
@@ -271,23 +269,54 @@ void loop() {
     delay(2000);
   }
   else if (game == "stac") {
-    if(!digitalRead(buttonPin)) { // if button press
-      new_level = tower[tower_y] & tower[tower_y + 1]; // new level is then what the heck am I doing?
-    }
+    while(!breakLoop) {
+      if (millis() - lastTime > waitTime) {
+        lastTime = millis();
+        if (direction == 1) {
+          if (!bitRead(tower[0], byteToXY(0))) { // if we're not at the edge
+            tower[0] = tower[0] << 1; // move it left
+          } else { // else we're at the edge
+            direction = 0;
+          }
+        } else {
+          if (!bitRead(tower[0], byteToXY(7))) { // and then repeat everything
+            tower[0] = tower[0] >> 1;
+          } else {
+            direction = 1;
+          }
+        }
 
-    if (direction == 1) {
-      if (!bitRead(tower[tower_y], byteToXY(0))) { // if we're not at the edge
-        tower[tower_y] = tower[tower_y] << 1; // move it left
-      } else { // else we're at the edge
-        direction = 0;
+        for (int i = 0; i < 7; i++) {
+          matrix.setRow(0, i+1, tower[i]);
+        }
       }
-    } else {
-      if (!bitRead(tower[tower_y], byteToXY(7))) { // and then repeat everything
-        tower[tower_y] = tower[tower_y] >> 1;
-      } else {
-        direction = 1;
+
+
+      if(!digitalRead(buttonPin)) { // if button press
+        tower[1] &= tower[0]; // AND the moving level and top stationary level
+        tower[0] = tower[1]; // actually update the top level
+        
+        if(tower[1] == B00000000) { // if there is nothing on the top level
+          matrix.clearDisplay(0); // cear display 0
+
+          int onesDigit = score % 10;
+          int tensDigit = (score / 10) % 10; // because result is int it removes stuff after decimal
+          for (int i = 0; i < 5; i++) { // b/c we only have 5 rows of number to display
+            matrix.setRow(0, i+1, (digits[tensDigit][i] << 4) | digits[onesDigit][i]); // this _should_ work. Maybe. Honestly tho it prob won't
+          }
+          breakLoop = true; // you lose boo hoo
+          // display score here somewhere. Use blackjack method?
+        }
+        score++;
+
+        // tower[6] is base
+        // tower[0] is the moving level
+        // only 7 items (0-6) b/c top one is always empty/blank/off
+        for (int i = 6; i > 1; i--) { // move everything down a level
+          tower[i] = tower[i-1];
+        }
       }
+      delay(50);
     }
-    delay(100);
   }
 }
