99103802e38b0e2a3ee8903364ff5fc2ec9bf70b
danielbrownmsm <daniel.brown@msmrockets.org>
2022-09-02 20:00:48 -0500

    rename everything to be not bad as suggested by Big Justin

--- mainLoop/mainLoop.ino
+++ mainLoop/mainLoop.ino
@@ -1,3 +1,21 @@
+class Game
+{
+private:
+    /* data */
+public:
+    Game(/* args */);
+    ~Game();
+    bool Loop(int x_val, int y_val, bool sw_val);
+};
+
+Game::Game(/* args */)
+{
+}
+
+Game::~Game()
+{
+}
+
 void setup() {
 
 }
