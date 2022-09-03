72293f1bd8f4e64f53fe983c3af5e1eb111fed5c
danielbrownmsm <daniel.brown@msmrockets.org>
2022-09-02 19:22:56 -0500

    created a base class for all games (I think. Still not sure how C++ inheritance works)

---
+++ matrixGame/game.ino
@@ -0,0 +1,17 @@
+class Game
+{
+private:
+    /* data */
+public:
+    Game(/* args */);
+    ~Game();
+    bool Loop(int x_val, int y_val, bool sw_val);
+};
+
+Game::Game(/* args */)
+{
+}
+
+Game::~Game()
+{
+}
