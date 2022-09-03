41ac244315c7901012d27e919e32fc40a3fb756b
danielbrownmsm <daniel.brown@msmrockets.org>
2022-09-02 20:11:49 -0500

    mainLoop is now a blank slate

--- mainLoop/mainLoop.ino
+++ mainLoop/mainLoop.ino
@@ -1,25 +1,7 @@
-class Game
-{
-private:
-    /* data */
-public:
-    Game(/* args */);
-    ~Game();
-    bool Loop(int x_val, int y_val, bool sw_val);
-};
-
-Game::Game(/* args */)
-{
-}
-
-Game::~Game()
-{
-}
-
 void setup() {
 
 }
 
 void loop() {
-
+    
 }
