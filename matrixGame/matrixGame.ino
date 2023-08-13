52821ff1bb3552024315ccdf8b916b0ef875690e
danielbrownmsm <daniel.brown@msmrockets.org>
2023-08-12 19:06:14 -0500

    start planning games out again (again, again)

---
+++ matrixGame/matrixGame.ino
@@ -0,0 +1,66 @@
+
+canyon runner:
+ left right movement
+ increase speed as go through
+ decrease width as go through
+ if player collide with wall then end
+ show score
+ back to menu
+
+calculator:
+ have player select first number (select tens digit, then ones digit)
+ have player select operation
+ have player select second number
+ output result of calculation, rounded/truncated to fit the two digits we have
+ if player clicks enter then go to first step, but if player does right stick then set first number to result of calculation and go to operation select
+
+guessing game?
+ have player select number (similar to calculator)
+ then show higher or lower arrows depending on if the number to guess is higher or lower
+ rinse and repeat
+
+
+
+block stack
+21/blackjack
+coin flip
+tron
+battleship?
+google dino game
+tetris
+options menu
+reaction timer
+pinball
+snake
+paint
+yahtzee?
+timer?????
+poker?
+pokemon???
+unblock me
+tic-tac-toe (knoghts and crosses)
+breakout (Atari)
+microtransactions
+John Conways GoL
+maze
+RPG???
+some music making thing
+support for writing programs on the console
+pong
+flappy bird
+crossy road
+simon says (memory game)
+a screensaver (why the heck would you want this tho?)
+rythym game (ex beatsaber)
+racing game (obviously 2D)
+some random dice game (what is that suppposed to mean? idk)
+bowling
+movies (stored in EEPROM) (how and why the heck? it's literally an 8x8 matrix and all one color with only basic sound support. If you really want to waste your time doing this then go ahead but until then)
+a platformer
+aliens (galaga)
+a clicker
+temple-run-but-not
+connect-4
+checkers???
+gravity-guy-but-not?
+party games (the heck does that mean?)
